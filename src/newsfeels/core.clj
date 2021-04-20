6cfae46a6d6246c0dfc25718d9f6522174ec8676
Marcela Poffald <mpoffald@gmail.com>
2021-04-20 13:17:15 -0400

    start building a system w/ a nytimes component

--- src/newsfeels/core.clj
+++ src/newsfeels/core.clj
@@ -1,6 +1,39 @@
-(ns newsfeels.core)
+(ns newsfeels.core
+  (:require
+   [clj-http.client :as client]
+   [com.stuartsierra.component :as component]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def domain "https://api.nytimes.com/")
+
+(defn get-most-popular
+  [client period]
+  (let [{:keys [api-key]} client])
+  (client/get (str domain "svc/mostpopular/v2/emailed/" period ".json")
+              {:as :json
+               :query-params {"api-key" api-key}}))
+
+(defrecord NyTimesClient
+    []
+    component/Lifecycle
+
+    (start [component]
+      (println "Starting NyTimesClient")
+
+      (let [{:keys [api-key]}
+            (clojure.edn/read-string (slurp "secrets/secrets.edn"))]
+        (assoc component :api-key api-key)))
+
+    (stop [component]
+      (println "Stopping NyTimesClient")
+      (assoc component :api-key nil)))
+
+(defn nytimes
+  [config]
+  (map->NyTimesClient config))
+
+(defn system
+  []
+  (component/system-map
+   :nytimes (nytimes {})))
+
+(def example-system (component/start (system)))
