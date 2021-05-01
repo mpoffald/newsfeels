8756dda094830bf49092096e1ccba3ccfff34126
Marcela Poffald <mpoffald@gmail.com>
2021-04-30 21:56:14 -0400

    whoops, need this too

---
+++ src/newsfeels/utils.clj
@@ -0,0 +1,21 @@
+(ns newsfeels.utils
+  (:require
+   [clojure.edn :as edn]))
+
+(defn get-config
+  []
+  (edn/read-string (slurp "config/config.edn")))
+
+;; TODO there must be a better way to do this
+(defn prepare-secrets
+  "Obscures secrets so they don't get printed in stack traces"
+  []
+  (let [secrets (edn/read-string (slurp "secrets/secrets.edn"))]
+    (into {}
+          (map (fn [[k s]]
+                 [k  (constantly s)]))
+          secrets)))
+
+(defn use-secret
+  [secret-fn]
+  (secret-fn))
